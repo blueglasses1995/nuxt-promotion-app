fc3f80847aee0dcb2c4a9b1825ee5a31c8d1ab21
blueglasses1995 <blueglasses1995@gmail.com>
2022-01-11 21:13:15 +0900

    first commit

---
+++ pages/cv.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <iframe :style="{width: '100%', height: '800px'}" src="/bishop_cv.pdf"></iframe>
+  </div>
+</template>
+<script>
+export default {
+  head: {
+    title: 'Toshiki Matsukuma CV | Toshiki Matsukuma'
+  }
+}
+</script>
---
+++ server/keys/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // DB_URI: process.env.DB_URI,
+  DB_URI: 'mongodb+srv://testuser:test@cluster0.fhzhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  // SESSION_SECRET: process.env.SESSION_SECRET
+  SESSION_SECRET: '43598240'
+}
